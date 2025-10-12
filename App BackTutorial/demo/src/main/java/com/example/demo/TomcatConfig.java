82d173b90e41edd0583c749ac75c7eecb4939889
Juan Antonio Tora Canovas <juanantonio.tora@gmail.com>
2025-10-12 10:13:51 +0200

    feat: configuraciones de puertos HTTPS y HTTP

---
+++ App BackTutorial/demo/src/main/java/com/example/demo/TomcatConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server -> {
+            Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+            connector.setPort(9090); // HTTP adicional
+            server.addAdditionalTomcatConnectors(connector);
+        };
+    }
+}
